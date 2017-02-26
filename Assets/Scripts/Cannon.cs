af1488287e78376a7d82fa5be9c8ee9169eb143b
Nick Warwick <n_warwick@yahoo.ca>
2017-02-26 14:46:40 -0800

    Added firing effect

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -1,42 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.Audio;
 
-public class Cannon : MonoBehaviour {
+public class Cannon : MonoBehaviour
+{
 
-	public float swizzleSpeed = 5f;
-	public GameObject bullet;
-	public Transform bulletSpawn;
-	//public Text bulletCountText;
-	//private int bulletCount = 0;
-	public AudioManager am;
+    public float swizzleSpeed = 5f;
+    public GameObject bullet;
+    public Transform bulletSpawn; // Bullet spawn locaiton
+    public Transform firingEffectSpawn; // Firing effect spawn location
+    public Transform firingEffectPrefab; // The firing effect prefab
+    //public Text bulletCountText;
+    //private int bulletCount = 0;
+    public AudioManager am;
 
-	public AudioClip clip;
-    private AudioSource source;
-	
-	// Use this for initialization
-	void Start () {
-		
-		if(bullet == null){
-			//cannon = GameObject.Find("Bullet").gameObject;
-		}
+    // Use this for initialization
+    void Start()
+    {
 
-		if(am == null){
-			am = GameObject.Find("AudioManager").GetComponent<AudioManager>();
-		}
+        if (bullet == null)
+        {
+            //cannon = GameObject.Find("Bullet").gameObject;
+        }
 
-	}
+        if (am == null)
+        {
+            am = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+        }
 
-	public void AimToward(Vector3 direction) {
-		//transform.right = direction;
-		transform.LookAt(direction);
-	}
+    }
 
-	public void Fire()
-	{
-		Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
-		am.PlaySound("Firing");
-	}
+    public void AimToward(Vector3 direction)
+    {
+        //transform.right = direction;
+        transform.LookAt(direction);
+    }
+
+    public void Fire()
+    {
+        Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+        am.PlaySound("Firing");
+		Effect();
+    }
+
+	// Firing effect
+    void Effect()
+    {
+		Transform effectInstance = (Transform) Instantiate(firingEffectPrefab, firingEffectSpawn.position, firingEffectSpawn.rotation);
+		effectInstance.parent = firingEffectSpawn;
+        effectInstance.GetComponent<ParticleSystem>().Play();
+		Destroy(effectInstance.gameObject, 1f); // Destroy after 0.5f time
+    }
 }
