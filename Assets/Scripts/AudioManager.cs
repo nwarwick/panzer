6d8afcd2b5c452ff158b8039df1b07e57885844e
Nick Warwick <n_warwick@yahoo.ca>
2017-02-25 19:37:50 -0800

    Added audio manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.SerializableAttribute]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    private AudioSource source;
+
+
+    public void SetSource(AudioSource newSource)
+    {
+        source = newSource;
+        source.clip = clip;
+    }
+
+    public void Play()
+    {
+		source.Play();
+    }
+}
+
+public class AudioManager : MonoBehaviour
+{
+
+	public static AudioManager instance;
+
+	[SerializeField]
+    Sound[] sounds;
+
+	void Awake()
+	{
+		if(instance != null)
+		{
+			Debug.LogError("Too many audio managers in this scene!");
+		} else {
+			instance = this;
+		}
+	}
+
+	
+	void Start()
+	{
+		for(int i = 0; i < sounds.Length; i ++)
+		{
+			GameObject gObj = new GameObject("Sound_" + i + "_" + sounds[i].name);
+			gObj.transform.SetParent(this.transform);
+			sounds[i].SetSource( gObj.AddComponent<AudioSource>() );
+
+		}
+
+	}
+
+	public void PlaySound(string _name) 
+	{
+		for(int i = 0; i < sounds.Length; i ++)
+		{
+			if(sounds[i].name == _name)
+			{
+				sounds[i].Play();
+				return;
+			}
+		}
+
+		Debug.Log("AudioManager: " + _name + " Sound not found");
+	}
+}
--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class Cannon : MonoBehaviour {
 
@@ -10,14 +11,22 @@ public class Cannon : MonoBehaviour {
 	public Transform bulletSpawn;
 	//public Text bulletCountText;
 	//private int bulletCount = 0;
+	public AudioManager am;
 
-
+	public AudioClip clip;
+    private AudioSource source;
+	
 	// Use this for initialization
 	void Start () {
 		
 		if(bullet == null){
 			//cannon = GameObject.Find("Bullet").gameObject;
 		}
+
+		if(am == null){
+			am = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+		}
+
 	}
 
 	public void AimToward(Vector3 direction) {
@@ -28,21 +37,6 @@ public class Cannon : MonoBehaviour {
 	public void Fire()
 	{
 		Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+		am.PlaySound("Firing");
 	}
-	
-	/*// Update is called once per frame
-	void Update () {
-		if (Input.GetKey(KeyCode.Q))
-		{
-			cannon.transform.Rotate(0f, -swizzleSpeed * Time.deltaTime, 0f);
-		}
-		if (Input.GetKey(KeyCode.E))
-		{
-			canon.transform.Rotate (0f, swizzleSpeed * Time.deltaTime, 0f);
-		}
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
-		}
-	}*/
 }
