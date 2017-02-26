8d06076bb4b9a9d18b0205473e259aeb2b12e6ca
Nick Warwick <n_warwick@yahoo.ca>
2017-02-25 17:56:50 -0800

    Added mouse aiming for cannon

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cannon : MonoBehaviour {
+
+	public float swizzleSpeed = 5f;
+	public GameObject bullet;
+	public Transform bulletSpawn;
+	//public Text bulletCountText;
+	//private int bulletCount = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		if(bullet == null){
+			//cannon = GameObject.Find("Bullet").gameObject;
+		}
+	}
+
+	public void AimToward(Vector3 direction) {
+		//transform.right = direction;
+		transform.LookAt(direction);
+	}
+
+	public void Fire()
+	{
+		Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+	}
+	
+	/*// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Q))
+		{
+			cannon.transform.Rotate(0f, -swizzleSpeed * Time.deltaTime, 0f);
+		}
+		if (Input.GetKey(KeyCode.E))
+		{
+			canon.transform.Rotate (0f, swizzleSpeed * Time.deltaTime, 0f);
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+		}
+	}*/
+}
---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour {
+	public float forwardSpeed = 15f;
+	public float backwardSpeed = 5f;
+	public float turnRate = 200f;
+
+	public GameObject cannonObject;
+	public Cannon cannon;
+
+	public Vector3 moveDirection = Vector3.zero;
+
+	public void Start() 
+	{
+		
+	}
+
+	void Update() {
+		// Move forward
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(0, 0, Time.deltaTime * forwardSpeed);
+		}
+
+		// Move backward
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(0, 0, -(Time.deltaTime * backwardSpeed));
+		}
+
+		// Turn left
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(0, -(Time.deltaTime * turnRate), 0);
+		}
+
+		// Turn right
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(0, Time.deltaTime * turnRate, 0);
+		}
+
+
+		Ray cameraRay;                // The ray that is cast from the camera to the mouse position
+     	RaycastHit cameraRayHit;
+
+		// Cast a ray from the camera to the mouse cursor
+         cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+         
+         // If the ray strikes an object...
+         if (Physics.Raycast(cameraRay, out cameraRayHit)) 
+         {
+             // ...and if that object is the ground...
+             if(cameraRayHit.transform.tag=="Ground")
+             {
+				 Debug.Log("Hit the ground");
+                 // ...make the cube rotate (only on the Y axis) to face the ray hit's position 
+                 Vector3 targetPosition = new Vector3(cameraRayHit.point.x, cannonObject.transform.position.y, cameraRayHit.point.z);
+				 cannon.AimToward(targetPosition);
+             }
+         }
+
+		if (Input.GetMouseButtonDown (0)) {
+			cannon.Fire ();
+		}
+	}
+}
