045778757b73e340f6454f0169efaf16a9562da2
Nick Warwick <n_warwick@yahoo.ca>
2017-03-02 12:01:05 -0800

    Fixed some issues with hitting level hazards

---
+++ Assets/Scripts/KillOnContact.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillOnContact : MonoBehaviour {
+    public GameObject deathEffect;
+    
+	void Start()
+	{
+		
+	}
+
+	void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("HIT SOMETHING");
+        // Check if a hazard was hit
+        if(other.gameObject.tag == "Hazard"){
+            Player player = GetComponent<Player>();
+            Tank tank = GetComponent<Tank>();
+            // See if we are the player
+            if(player != null)
+            {
+                player.Kill();
+
+            }
+
+            if(tank != null)
+            {
+                tank.Explode();
+            }
+            
+        }
+    }
+}
