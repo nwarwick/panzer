bd3619ee83a50702b7095dc734e141ec372032c1
Nick Warwick <n_warwick@yahoo.ca>
2017-03-03 11:31:02 -0800

    Removed call to player.Kill()

--- Assets/Scripts/KillOnContact.cs
+++ Assets/Scripts/KillOnContact.cs
@@ -10,15 +10,9 @@ public class KillOnContact : MonoBehaviour {
         Debug.Log("HIT SOMETHING");
         // Check if a hazard was hit
         if(other.gameObject.tag == "Hazard"){
-            Player player = GetComponent<Player>();
+            
             Tank tank = GetComponent<Tank>();
-            // See if we are the player
-            if(player != null)
-            {
-                player.Kill();
-
-            }
-
+            
             if(tank != null)
             {
                 tank.Explode();
