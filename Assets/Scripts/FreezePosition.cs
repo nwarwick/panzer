62ff881ade65d35646227fac175ee424e49403e6
Nick Warwick <n_warwick@yahoo.ca>
2017-03-07 16:45:24 -0800

    Added a script to freeze positions of rigidbodies

---
+++ Assets/Scripts/FreezePosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezePosition : MonoBehaviour {
+
+	
+	void OnCollisionEnter(Collision other)
+	{
+		Debug.Log("Freezing position");
+		gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+	}
+	
+}
